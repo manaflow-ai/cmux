14d2322b9e71d612b178d6c1f7bf5d5b76085146
Assaf Ben-Amitai <assaf758@gmail.com>
2026-03-11 00:44:58 +0200

    fix: cert-bypass help syntax

--- CLI/cmux.swift
+++ CLI/cmux.swift
@@ -5051,8 +5051,7 @@ struct CMUXCLI {
               viewport <width> <height>
               geolocation|geo <latitude> <longitude>
               offline <true|false>
-              cert-bypass get
-              cert-bypass set <true|false>
+              cert-bypass [get | set <true|false>]
               trace <start|stop> [path]
               network <route|unroute|requests> ...
                 route <pattern> [--abort] [--body <text>]
