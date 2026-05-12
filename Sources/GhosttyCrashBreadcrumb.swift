ffd75be9bce0f74f914f1b4f13c0b61f79d9ed3c
austinpower1258 <austinwang115@gmail.com>
2026-05-12 10:56:48 -0700

    fix: use renamed crash breadcrumb annotation

--- Sources/GhosttyCrashBreadcrumb.swift
+++ Sources/GhosttyCrashBreadcrumb.swift
@@ -15,7 +15,7 @@ nonisolated enum GhosttyCrashBreadcrumb {
             .appendingPathComponent(".local/state/ghostty/crash", isDirectory: true)
     }
 
-    @concurrent
+    @Sendable
     nonisolated static func pendingCrashFromDefaultStorage() async -> PendingCrash? {
         pendingCrash()
     }
