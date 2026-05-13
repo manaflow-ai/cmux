836abb0396a55a0d5588b1d3ce457ce158b410d3
austinpower1258 <austinwang115@gmail.com>
2026-05-13 15:37:06 -0700

    Fix global search extension pane indexing

--- Sources/Search/GlobalSearchDocuments.swift
+++ Sources/Search/GlobalSearchDocuments.swift
@@ -34,7 +34,7 @@ enum GlobalSearchDocuments {
             kind = .browser
         case .markdown:
             kind = .markdown
-        case .terminal, .filePreview, .rightSidebarTool:
+        case .terminal, .extensionPane, .filePreview, .rightSidebarTool:
             kind = .title
         }
 
