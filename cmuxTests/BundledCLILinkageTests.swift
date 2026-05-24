8daae820ebab90d32ba4807ef14640bb04ef22e5
austinpower1258 <austinwang115@gmail.com>
2026-05-24 14:21:26 -0700

    fix: import app module in CLI forwarding tests

--- cmuxTests/BundledCLILinkageTests.swift
+++ cmuxTests/BundledCLILinkageTests.swift
@@ -3,6 +3,12 @@ import Dispatch
 import Foundation
 import XCTest
 
+#if canImport(cmux_DEV)
+@testable import cmux_DEV
+#elseif canImport(cmux)
+@testable import cmux
+#endif
+
 final class BundledCLILinkageTests: XCTestCase {
     deinit {}
 
