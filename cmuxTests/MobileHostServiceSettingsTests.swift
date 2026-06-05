7cc49883b1cc44871ca3b7ac3a3fc2c60a3e9763
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2026-06-05 16:21:03 -0700

    Remove mobile host legacy defaults (#5484)

--- cmuxTests/MobileHostServiceSettingsTests.swift
+++ cmuxTests/MobileHostServiceSettingsTests.swift
@@ -21,28 +21,4 @@ struct MobileHostServiceSettingsTests {
         defaults.set(false, forKey: MobileHostService.listeningEnabledDefaultsKey)
         #expect(!MobileHostService.isListeningEnabled(defaults: defaults))
     }
-
-    @Test func mobileHostListenerHonorsLegacyExplicitOptIn() throws {
-        let suiteName = "MobileHostServiceSettingsTests.Legacy.\(UUID().uuidString)"
-        let defaults = try #require(UserDefaults(suiteName: suiteName))
-        defer { defaults.removePersistentDomain(forName: suiteName) }
-
-        defaults.set(true, forKey: "cmuxMobilePairingHostEnabled")
-        #expect(MobileHostService.isListeningEnabled(defaults: defaults))
-
-        defaults.set(false, forKey: MobileHostService.listeningEnabledDefaultsKey)
-        #expect(!MobileHostService.isListeningEnabled(defaults: defaults))
-    }
-
-    @Test func mobileHostListenerHonorsLegacyBetaExplicitOptIn() throws {
-        let suiteName = "MobileHostServiceSettingsTests.LegacyBeta.\(UUID().uuidString)"
-        let defaults = try #require(UserDefaults(suiteName: suiteName))
-        defer { defaults.removePersistentDomain(forName: suiteName) }
-
-        defaults.set(true, forKey: "ios.beta.pairingHost.enabled")
-        #expect(MobileHostService.isListeningEnabled(defaults: defaults))
-
-        defaults.set(false, forKey: MobileHostService.listeningEnabledDefaultsKey)
-        #expect(!MobileHostService.isListeningEnabled(defaults: defaults))
-    }
 }
