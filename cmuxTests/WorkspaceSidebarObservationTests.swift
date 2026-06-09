112e59c5bc9b6d407ef7f21477aee7080e1e3e0f
austinpower1258 <austinwang115@gmail.com>
2026-06-09 12:53:48 -0700

    Fix sidebar observation test import

--- cmuxTests/WorkspaceSidebarObservationTests.swift
+++ cmuxTests/WorkspaceSidebarObservationTests.swift
@@ -1,6 +1,11 @@
 import Foundation
 import XCTest
+
+#if canImport(cmux_DEV)
+@testable import cmux_DEV
+#elseif canImport(cmux)
 @testable import cmux
+#endif
 
 final class WorkspaceSidebarObservationTests: XCTestCase {
     func testSidebarObservationPublisherEmitsForLateStatusSubscriber() {
