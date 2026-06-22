cd1caf5a1d113beeb1c577b989992f4db3536764
austinywang <austinwang115@gmail.com>
2026-06-22 03:09:21 -0700

    fix: clean up divider cache invalidation observers

--- Sources/PortalSplitDividerCacheInvalidator.swift
+++ Sources/PortalSplitDividerCacheInvalidator.swift
@@ -4,14 +4,18 @@ final class PortalSplitDividerCacheInvalidator {
     private var observations: [NSKeyValueObservation] = []
     private var notificationObservers: [NSObjectProtocol] = []
 
+    deinit {
+        invalidate()
+    }
+
     func observe(_ views: [NSView], onChange: @escaping () -> Void) {
         invalidate()
         notificationObservers = views.flatMap { view in
             view.postsFrameChangedNotifications = true
             view.postsBoundsChangedNotifications = true
             return [
-                NotificationCenter.default.addObserver(forName: NSView.frameDidChangeNotification, object: view, queue: .main) { _ in onChange() },
-                NotificationCenter.default.addObserver(forName: NSView.boundsDidChangeNotification, object: view, queue: .main) { _ in onChange() },
+                NotificationCenter.default.addObserver(forName: NSView.frameDidChangeNotification, object: view, queue: nil) { _ in onChange() },
+                NotificationCenter.default.addObserver(forName: NSView.boundsDidChangeNotification, object: view, queue: nil) { _ in onChange() },
             ]
         }
         observations = views.flatMap { view in
