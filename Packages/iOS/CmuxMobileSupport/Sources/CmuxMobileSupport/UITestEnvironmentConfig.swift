2e10a1a86db23153db998539ffaea2a6df7561da
Aziz Albahar <abdulaziz@albahar.net>
2026-06-23 22:10:33 -0700

    Document UI test environment config

--- Packages/iOS/CmuxMobileSupport/Sources/CmuxMobileSupport/UITestEnvironmentConfig.swift
+++ Packages/iOS/CmuxMobileSupport/Sources/CmuxMobileSupport/UITestEnvironmentConfig.swift
@@ -2,6 +2,9 @@
 public struct UITestEnvironmentConfig: Equatable, Sendable {
     private let environment: [String: String]
 
+    /// Creates a UI-test config from explicit environment values.
+    ///
+    /// - Parameter environment: Process-style environment keys and values.
     public init(environment: [String: String]) {
         self.environment = environment
     }
