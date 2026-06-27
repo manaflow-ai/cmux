2834052b743e419b82fd392603fc5afbdd4ccc6f
cmux <cmux@cmuxs-Mac-mini.local>
2026-06-27 03:55:40 -0700

    Allow hardware key commands in static tables

--- Packages/iOS/CmuxMobileTerminal/Sources/CmuxMobileTerminal/TerminalHardwareKeyCommand.swift
+++ Packages/iOS/CmuxMobileTerminal/Sources/CmuxMobileTerminal/TerminalHardwareKeyCommand.swift
@@ -4,5 +4,10 @@ import UIKit
 struct TerminalHardwareKeyCommand: Sendable {
     let input: String
     let modifierFlags: UIKeyModifierFlags
+
+    nonisolated init(input: String, modifierFlags: UIKeyModifierFlags) {
+        self.input = input
+        self.modifierFlags = modifierFlags
+    }
 }
 #endif
