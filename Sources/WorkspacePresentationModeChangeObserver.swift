d6cdcd8a62e34c600a2d731f58f8566299498454
Austin Wang <austinwang115@gmail.com>
2026-06-29 17:53:11 -0700

    Address minimal mode review feedback

--- Sources/WorkspacePresentationModeChangeObserver.swift
+++ Sources/WorkspacePresentationModeChangeObserver.swift
@@ -14,6 +14,9 @@ struct WorkspacePresentationModeChangeObserver: View {
         Color.clear
             .frame(width: 0, height: 0)
             .allowsHitTesting(false)
+            .onAppear {
+                onChange(isMinimalMode)
+            }
             .onChange(of: isMinimalMode) { _, newValue in
                 onChange(newValue)
             }
