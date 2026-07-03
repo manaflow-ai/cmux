6a6b694990bc9e15016a2973427dd2a2770cd2d1
Austin Wang <austinwang115@gmail.com>
2026-07-03 16:32:06 -0700

    Test primary terminal font parsing

---
+++ Packages/macOS/CmuxTerminalCore/Tests/CmuxTerminalCoreTests/GhosttyConfigFontFamilyTests.swift
@@ -0,0 +1,20 @@
+import CmuxTerminalCore
+import Testing
+
+@Suite
+struct GhosttyConfigFontFamilyTests {
+    @Test func parserUsesPrimaryFontFamilyAfterReset() {
+        var config = GhosttyConfig()
+
+        config.parse(
+            """
+            font-family = Old Mono
+            font-family =
+            font-family = JetBrains Mono
+            font-family = Hiragino Sans
+            """
+        )
+
+        #expect(config.fontFamily == "JetBrains Mono")
+    }
+}
