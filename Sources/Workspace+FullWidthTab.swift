738fec87eda798a3be64dd3c2c3fc4198fcd9ba3
Aziz Albahar <abdulaziz@albahar.net>
2026-07-07 13:14:50 -0700

    Preserve full width tab toggle success semantics

--- Sources/Workspace+FullWidthTab.swift
+++ Sources/Workspace+FullWidthTab.swift
@@ -5,7 +5,8 @@ extension Workspace {
     @discardableResult
     func toggleFullWidthTabMode(panelId: UUID) -> Bool {
         guard let paneId = paneId(forPanelId: panelId) else { return false }
-        _ = bonsplitController.toggleFullWidthTabMode(inPane: paneId)
+        let nextMode = !bonsplitController.isFullWidthTabMode(inPane: paneId)
+        guard bonsplitController.setFullWidthTabMode(nextMode, inPane: paneId) else { return false }
         focusPanel(panelId)
         return true
     }
