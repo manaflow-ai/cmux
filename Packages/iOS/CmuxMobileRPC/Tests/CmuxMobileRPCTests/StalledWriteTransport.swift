0705a31ee596e28efa55b30cdb111d23a3180f17
austinpower1258 <austinwang115@gmail.com>
2026-07-13 16:25:23 -0700

    test(ios): reproduce stalled RPC write connection loss

---
+++ Packages/iOS/CmuxMobileRPC/Tests/CmuxMobileRPCTests/StalledWriteTransport.swift
@@ -0,0 +1,41 @@
+import CMUXMobileCore
+import Foundation
+
+actor StalledWriteTransport: CmxByteTransport {
+    private var receiveWaiters: [CheckedContinuation<Data?, Never>] = []
+    private var sendWaiter: CheckedContinuation<Void, any Error>?
+    private var isClosed = false
+
+    func connect() async throws {}
+
+    func receive() async throws -> Data? {
+        if isClosed {
+            return nil
+        }
+        return await withCheckedContinuation { continuation in
+            receiveWaiters.append(continuation)
+        }
+    }
+
+    func send(_ data: Data) async throws {
+        try await withCheckedThrowingContinuation { continuation in
+            sendWaiter = continuation
+        }
+    }
+
+    func close() async {
+        guard !isClosed else { return }
+        isClosed = true
+        sendWaiter?.resume(throwing: CancellationError())
+        sendWaiter = nil
+        let waiters = receiveWaiters
+        receiveWaiters = []
+        for waiter in waiters {
+            waiter.resume(returning: nil)
+        }
+    }
+
+    func closed() -> Bool {
+        isClosed
+    }
+}
