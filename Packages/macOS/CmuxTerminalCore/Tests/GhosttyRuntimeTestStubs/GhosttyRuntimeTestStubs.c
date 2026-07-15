c9802b2bb4d617327b772865103a50822aafe73e
austinpower1258 <austinwang115@gmail.com>
2026-07-14 19:40:08 -0700

    test: make Ghostty runtime stubs weak fallbacks

--- Packages/macOS/CmuxTerminalCore/Tests/GhosttyRuntimeTestStubs/GhosttyRuntimeTestStubs.c
+++ Packages/macOS/CmuxTerminalCore/Tests/GhosttyRuntimeTestStubs/GhosttyRuntimeTestStubs.c
@@ -1,11 +1,11 @@
 #include "include/GhosttyRuntimeTestStubs.h"
 
-bool ghostty_surface_clear_selection(void *surface) {
+__attribute__((weak)) bool ghostty_surface_clear_selection(void *surface) {
     (void)surface;
     return false;
 }
 
-void *ghostty_surface_quicklook_font(void *surface) {
+__attribute__((weak)) void *ghostty_surface_quicklook_font(void *surface) {
     (void)surface;
     return 0;
 }
