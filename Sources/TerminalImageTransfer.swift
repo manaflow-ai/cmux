591ccd588525800657116564fc0bcd87f77839a6
austinywang <austinwang115@gmail.com>
2026-07-24 02:20:40 -0700

    Revert "refactor: remove blocking image prepare overload"
    This reverts commit 36f65f6b02d6d8df7a7c45cff9e611d8e53b213a.

--- Sources/TerminalImageTransfer.swift
+++ Sources/TerminalImageTransfer.swift
@@ -195,6 +195,13 @@ enum TerminalImageTransferPlanner {
         }
     }
 
+    static func prepare(
+        pasteboard: NSPasteboard,
+        mode: TerminalImageTransferMode
+    ) -> TerminalImageTransferPreparedContent {
+        prepareSynchronously(pasteboard: pasteboard, mode: mode)
+    }
+
     @MainActor
     static func prepare(
         pasteboard: NSPasteboard,
