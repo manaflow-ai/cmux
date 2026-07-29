d212472f2e5cc34170c3361b416289736cebf656
austinpower1258 <austinwang115@gmail.com>
2026-07-29 06:41:19 -0700

    refactor: split agent hibernation panel types

--- Sources/AgentHibernation/AgentHibernationPanelPhase.swift
+++ Sources/AgentHibernation/AgentHibernationPanelPhase.swift
@@ -1,15 +1,3 @@
-import Foundation
-
-struct AgentHibernationPanelState {
-    let agent: SessionRestorableAgentSnapshot
-    let hibernatedAt: Date
-    let lastActivityAt: Date
-
-    var agentDisplayName: String {
-        agent.agentDisplayName
-    }
-}
-
 enum AgentHibernationPanelPhase {
     case live
     case terminating(AgentHibernationPanelState)
@@ -51,20 +39,5 @@ enum AgentHibernationPanelPhase {
         case .live, .hibernated:
             false
         }
-    }
-}
-
-enum AgentHibernationResumePreparation: Equatable {
-    case unavailable
-    case resumed(queuedStartupInput: Bool)
-
-    var didResume: Bool {
-        if case .resumed = self { return true }
-        return false
-    }
-
-    var queuedStartupInput: Bool {
-        if case .resumed(let queuedStartupInput) = self { return queuedStartupInput }
-        return false
     }
 }
