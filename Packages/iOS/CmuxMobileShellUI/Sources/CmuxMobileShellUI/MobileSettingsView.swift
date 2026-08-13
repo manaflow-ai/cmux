952586ecc5aa65aa460e87667634e658d9ed23e1
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2026-08-13 13:08:01 -0700

    fix(ios): restore toast settings binding

--- Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/MobileSettingsView.swift
+++ Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/MobileSettingsView.swift
@@ -63,6 +63,7 @@ struct MobileSettingsView: View {
 
     var body: some View {
         @Bindable var displaySettings = displaySettings
+        @Bindable var toasts = toasts
         return NavigationStack {
             Form {
                 if initialFocus == .connectionMethod {
