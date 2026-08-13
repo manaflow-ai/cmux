98ea04d735b728c03ce52eea1b8775580233ee1f
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2026-08-12 22:18:54 -0700

    iOS: permanently disable mobile toasts (#10087)
    * test(ios): keep legacy toast preference disabled
    
    * fix(ios): permanently disable mobile toasts
    
    * docs(ios): document toast presenter policy

--- Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/MobileSettingsView.swift
+++ Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/MobileSettingsView.swift
@@ -62,7 +62,6 @@ struct MobileSettingsView: View {
 
     var body: some View {
         @Bindable var displaySettings = displaySettings
-        @Bindable var toasts = self.toasts
         return NavigationStack {
             Form {
                 if initialFocus == .connectionMethod {
@@ -246,13 +245,6 @@ struct MobileSettingsView: View {
                     }
                     .accessibilityIdentifier("MobileSettingsTerminalFilesChip")
 
-                    Toggle(isOn: $toasts.isEnabled) {
-                        Text(L10n.string(
-                            "mobile.settings.beta.toasts",
-                            defaultValue: "Toasts"
-                        ))
-                    }
-                    .accessibilityIdentifier("MobileSettingsToastsEnabled")
                 }
 
                 #if DEBUG
