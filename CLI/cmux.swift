35441e99d708740835134bede0c48a7bb1c00814
austinpower1258 <austinwang115@gmail.com>
2026-08-26 03:24:47 -0700

    fix(ci): clear merged-main Swift warnings and drop regression

--- CLI/cmux.swift
+++ CLI/cmux.swift
@@ -33240,7 +33240,7 @@ export default CMUXSessionRestore;
         } else {
             Self.subcommandActions[subcommand] ?? .noop
         }
-        var hookResponse = cursorShellNeedsApproval
+        let hookResponse = cursorShellNeedsApproval
             ? AgentHookNotificationPolicy.cursorNativeApprovalResponse
             : "{}"
 #if DEBUG
@@ -35835,7 +35835,7 @@ export default CMUXSessionRestore;
             event["tool_input"] = source == "cursor"
                 ? sanitizedCursorFeedToolInput(toolInput)
                 : toolInput
-        } else if let cursorShellCommand {
+        } else if cursorShellCommand != nil {
             event["tool_input"] = [
                 "command": "<redacted>"
             ]
