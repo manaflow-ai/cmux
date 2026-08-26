e1d2298582b3d69a55326ba2f8b96c53c3818e4a
austinpower1258 <austinwang115@gmail.com>
2026-08-26 02:56:22 -0700

    fix(plugins): bound snapshot staging and socket buffers

--- Sources/TerminalController.swift
+++ Sources/TerminalController.swift
@@ -1833,8 +1833,7 @@ class TerminalController {
         let lineReader = ControlClientAsyncLineReader(
             socket: socket,
             initialLimits: initialReadLimits,
-            authorizationRevocationSignal: authorizationRevocationSignal,
-            maximumBufferedBytes: 4 * 1024 * 1024
+            authorizationRevocationSignal: authorizationRevocationSignal
         )
         let writer = ControlClientAsyncWriter(socket: socket)
         let rateLimiter = ControlClientRateLimiter()
