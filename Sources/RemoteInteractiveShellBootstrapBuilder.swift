a7a0bfb0c424317286d5eb55560454945c1e076a
daniel raffel <daniel.raffel@gmail.com>
2026-08-28 15:16:54 -0700

    fix: give resource loader explicit file ownership

--- Sources/RemoteInteractiveShellBootstrapBuilder.swift
+++ Sources/RemoteInteractiveShellBootstrapBuilder.swift
@@ -1,32 +1,6 @@
 import CmuxFoundation
 import Foundation
 
-struct RemoteSessionBundledResourceLoader {
-    let resourceURL: URL?
-    let fileManager: FileManager
-
-    init(
-        resourceURL: URL? = Bundle.main.resourceURL,
-        fileManager: FileManager = .default
-    ) {
-        self.resourceURL = resourceURL
-        self.fileManager = fileManager
-    }
-
-    func codexWrapperScript() -> String? {
-        guard let resourceURL else { return nil }
-        let url = resourceURL
-            .appendingPathComponent("bin", isDirectory: true)
-            .appendingPathComponent("cmux-codex-wrapper", isDirectory: false)
-        guard fileManager.fileExists(atPath: url.path),
-              let data = try? Data(contentsOf: url),
-              let contents = String(data: data, encoding: .utf8) else {
-            return nil
-        }
-        return contents
-    }
-}
-
 enum RemoteInteractiveShellBootstrapBuilder {
     static func script(
         remoteRelayPort: Int,
