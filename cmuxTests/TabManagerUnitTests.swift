5cf6e76587b4a205221921900d4df200a65599e9
Austin Wang <austinwang115@gmail.com>
2026-09-01 11:46:07 -0700

    revert: remove unnecessary test deinit

--- cmuxTests/TabManagerUnitTests.swift
+++ cmuxTests/TabManagerUnitTests.swift
@@ -939,8 +939,6 @@ struct TabManagerSplitZoomCleanupTests {
 
 @MainActor
 final class TabManagerWorkspaceOwnershipTests: XCTestCase {
-    deinit {}
-
     func testCloseWorkspaceIgnoresWorkspaceNotOwnedByManager() {
         let manager = TabManager()
         _ = manager.addWorkspace()
