8c988d8878bd1bea2e6e0937c2a7046f2c2df9c0
austinpower1258 <austinwang115@gmail.com>
2026-09-03 21:56:15 -0700

    test: keep mobile surface mapping oracle independent

--- cmuxTests/MobileSurfaceKindMappingTests.swift
+++ cmuxTests/MobileSurfaceKindMappingTests.swift
@@ -24,10 +24,10 @@ import Testing
         .extensionBrowser: "extensionBrowser",
         .workspaceTodo: "todo",
         .cloudVMLoading: "cloudVMLoading",
-        .simulator: MobileSurfaceKind.simulator.rawValue,
-        .notifications: MobileSurfaceKind.notifications.rawValue,
-        .mobilePairing: MobileSurfaceKind.mobilePairing.rawValue,
-        .accountSignIn: MobileSurfaceKind.accountSignIn.rawValue,
+        .simulator: "simulator",
+        .notifications: "notifications",
+        .mobilePairing: "mobilePairing",
+        .accountSignIn: "accountSignIn",
     ]
 
     @Test func everyPanelTypeMapsToItsCanonicalWireKind() throws {
