8e9ebb0df1b2c285659fdf150533aa4bdf3c90bc
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2026-09-12 06:24:39 -0700

    Revert "ios: disable navigation toolbar in preview"
    This reverts commit e8f0d7747740acdb76a67967c85772d8c75e3053.

--- Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/WorkspaceListLayoutPreviewView.swift
+++ Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/WorkspaceListLayoutPreviewView.swift
@@ -644,8 +644,6 @@ public struct WorkspaceListLayoutPreviewView: View {
             host: "Visual Mock Mac",
             connectionStatus: fixtureConnectionStatus,
             navigationStyle: usesSidebarSelectionFixture ? .sidebar : .push,
-            showsNavigationToolbar: false,
-            usesExternalSharedToolbar: true,
             wrapWorkspaceTitles: false,
             previewLineLimit: MobileDisplaySettings.defaultWorkspacePreviewLineCount,
             unreadIndicatorLeftShift: MobileDisplaySettings.defaultUnreadIndicatorLeftShift,
