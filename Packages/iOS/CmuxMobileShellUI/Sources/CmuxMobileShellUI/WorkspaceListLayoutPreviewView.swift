8303b62692e32be4c6785c5c24cff2969002dd57
Lawrence Chen <54008264+lawrencecchen@users.noreply.github.com>
2026-09-12 06:24:40 -0700

    Revert "ios: fix preview argument ordering"
    This reverts commit 38a8c61c62f936ca719d27a2b90d599b9c04e398.

--- Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/WorkspaceListLayoutPreviewView.swift
+++ Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/WorkspaceListLayoutPreviewView.swift
@@ -655,7 +655,6 @@ public struct WorkspaceListLayoutPreviewView: View {
                 }
             },
             createWorkspace: {},
-            canCreateWorkspace: false,
             createWorkspaceInGroup: reorderEnabled ? { _ in } : nil,
             createWorkspaceGroup: reorderEnabled ? {} : nil,
             macSelection: $macSelection,
@@ -738,6 +737,7 @@ public struct WorkspaceListLayoutPreviewView: View {
             setWorkspaceSortMode: nil,
             workspaceComputerPriority: fixtureComputerPriority,
             setWorkspaceComputerPriority: nil,
+            canCreateWorkspace: false,
             filterState: filterState,
             searchText: searchText
         )
