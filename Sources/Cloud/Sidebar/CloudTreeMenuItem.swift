66a5144f40a11c04b0174f057a66600f791e96fc
austinpower1258 <austinwang115@gmail.com>
2026-09-13 16:43:25 -0700

    fix: satisfy sidebar menu item lifecycle lint

--- Sources/Cloud/Sidebar/CloudTreeMenuItem.swift
+++ Sources/Cloud/Sidebar/CloudTreeMenuItem.swift
@@ -15,6 +15,8 @@ final class CloudTreeMenuItem: NSMenuItem {
         target = self
     }
 
+    deinit {}
+
     @available(*, unavailable)
     required init(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
