122066c48483109fb88a757b6a4e739059b53871
austinpower1258 <austinwang115@gmail.com>
2026-09-12 23:07:33 -0700

    fix: resolve dock surface UUID through tab index

--- Sources/DockSplitStore+PaneFocus.swift
+++ Sources/DockSplitStore+PaneFocus.swift
@@ -34,7 +34,7 @@ extension DockSplitStore {
         if panels[sourceID] != nil {
             return sourceID
         }
-        if let mappedPanelID = surfaceIdToPanelId[sourceID],
+        if let mappedPanelID = surfaceIdToPanelId.first(where: { $0.key.uuid == sourceID })?.value,
            panels[mappedPanelID] != nil {
             return mappedPanelID
         }
