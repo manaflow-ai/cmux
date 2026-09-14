84f6baec903c7ae2b3596508b66fea1ea5f0e2e6
austinpower1258 <austinwang115@gmail.com>
2026-09-13 21:34:28 -0700

    fix: override private extension visibility for web-view ownership

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -19954,7 +19954,7 @@ private extension NSWindow {
         return nil
     }
 
-    static func cmuxOwningWebView(
+    internal static func cmuxOwningWebView(
         for responder: NSResponder,
         in window: NSWindow,
         event: NSEvent?
