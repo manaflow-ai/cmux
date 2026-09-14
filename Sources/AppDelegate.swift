7344324b7c075169c4d103c24dbba194bc863f2e
Austin Wang <austinwang115@gmail.com>
2026-09-13 21:34:26 -0700

    Allow the existing keyboard replay guard to resolve its WebView helper

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -19916,7 +19916,7 @@ private extension NSWindow {
         return nil
     }
 
-    fileprivate static func cmuxOwningWebView(
+    static func cmuxOwningWebView(
         for responder: NSResponder,
         in window: NSWindow,
         event: NSEvent?
