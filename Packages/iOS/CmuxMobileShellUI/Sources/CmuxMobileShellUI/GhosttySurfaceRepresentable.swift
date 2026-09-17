e1debc77bdb4a3fd6f3ad864df1a10edb0e6c02d
Abdulaziz Albahar <67667005+azooz2003-bit@users.noreply.github.com>
2026-09-17 12:49:32 -0700

    Fix presentation callback type inference

--- Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/GhosttySurfaceRepresentable.swift
+++ Packages/iOS/CmuxMobileShellUI/Sources/CmuxMobileShellUI/GhosttySurfaceRepresentable.swift
@@ -593,15 +593,19 @@ struct GhosttySurfaceRepresentable: UIViewRepresentable {
                         let observedInput = frame.appliedInputSequence
                         let observedReceipt = chunk.receivedAtNanos
                         let observedStream = chunk.streamToken
-                        surfaceView.onOutputPresentation = chunk.latencyMetricsEligible ? { [weak store] in
-                            store?.terminalOutputDidPresent(
-                                surfaceID: surfaceID,
-                                streamToken: observedStream,
-                                inputSequence: observedInput,
-                                receivedAtNanos: observedReceipt,
-                                latencyMetricsEligible: true
-                            )
-                        } : nil
+                        if chunk.latencyMetricsEligible {
+                            surfaceView.onOutputPresentation = { @MainActor @Sendable [weak store] in
+                                store?.terminalOutputDidPresent(
+                                    surfaceID: surfaceID,
+                                    streamToken: observedStream,
+                                    inputSequence: observedInput,
+                                    receivedAtNanos: observedReceipt,
+                                    latencyMetricsEligible: true
+                                )
+                            }
+                        } else {
+                            surfaceView.onOutputPresentation = nil
+                        }
                         let applied = await self.applyVerifiedRenderGrid(
                             frame,
                             chunk: chunk,
@@ -738,15 +742,19 @@ struct GhosttySurfaceRepresentable: UIViewRepresentable {
                     let observedInput = chunk.sourceRenderGridFrame?.appliedInputSequence
                     let observedReceipt = chunk.receivedAtNanos
                     let observedStream = chunk.streamToken
-                    surfaceView.onOutputPresentation = chunk.latencyMetricsEligible ? { [weak store] in
-                        store?.terminalOutputDidPresent(
-                            surfaceID: surfaceID,
-                            streamToken: observedStream,
-                            inputSequence: observedInput,
-                            receivedAtNanos: observedReceipt,
-                            latencyMetricsEligible: true
-                        )
-                    } : nil
+                    if chunk.latencyMetricsEligible {
+                        surfaceView.onOutputPresentation = { @MainActor @Sendable [weak store] in
+                            store?.terminalOutputDidPresent(
+                                surfaceID: surfaceID,
+                                streamToken: observedStream,
+                                inputSequence: observedInput,
+                                receivedAtNanos: observedReceipt,
+                                latencyMetricsEligible: true
+                            )
+                        }
+                    } else {
+                        surfaceView.onOutputPresentation = nil
+                    }
                     #if DEBUG
                     surfaceView.markLatencyAppliedSequence(latencySequence)
                     MobileLatencyTrace.stampElapsed(
