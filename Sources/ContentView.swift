f1ef8899402af15036b64de5bd255d79ac131dfc
austinpower1258 <austinwang115@gmail.com>
2026-09-17 23:31:19 -0700

    chore: keep ContentView within file budget

--- Sources/ContentView.swift
+++ Sources/ContentView.swift
@@ -3467,11 +3467,7 @@ struct ContentView: View {
             }
         })
 
-        view = AnyView(view.ignoresSafeArea())
-        view = AnyView(view.overlay {
-            WindowContentOverlayBrowserHost()
-                .allowsHitTesting(false)
-        })
+        view = AnyView(view.ignoresSafeArea().overlay(WindowContentOverlayBrowserHost().allowsHitTesting(false)))
         view = AnyView(view.sheet(isPresented: $isFeedbackComposerPresented) {
             SidebarFeedbackComposerSheet()
         })
