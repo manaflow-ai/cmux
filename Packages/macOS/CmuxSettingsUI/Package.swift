1a7aca8032e63fc7892987fe8b45d3b888b15be1
Leo <cheerleaderleo@outlook.com>
2026-09-19 11:25:33 -0700

    Add Settings onboarding localization resource

--- Packages/macOS/CmuxSettingsUI/Package.swift
+++ Packages/macOS/CmuxSettingsUI/Package.swift
@@ -4,6 +4,7 @@ import PackageDescription
 
 let package = Package(
     name: "CmuxSettingsUI",
+    defaultLocalization: "en",
     platforms: [
         .macOS(.v14),
     ],
@@ -28,6 +29,7 @@ let package = Package(
             ],
             resources: [
                 .copy("Resources/CustomSidebars"),
+                .process("Resources/Localizable.xcstrings"),
             ]
         ),
         .testTarget(
