1c8b7a2ebbac88d87f1fd669b6e8ef03a3768cd0
Aliyan Sajid <aliyansajid127@gmail.com>
2026-09-19 04:29:11 -0400

    test: cover deferred termination on the Cmd+Q quit path (#10788)

--- Sources/AppDelegate.swift
+++ Sources/AppDelegate.swift
@@ -14415,7 +14415,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
             hasDirtyWorkspaces: hasQuitConfirmationDirtyWorkspaces(),
             isDevBuild: BuildFlavor.current == .dev
         ) {
-            NSApp.terminate(nil)
+            Self.requestApplicationTermination()
             return true
         }
 
@@ -14428,7 +14428,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, UNUserNotificationCent
                 // Mark as confirmed so applicationShouldTerminate does not show a
                 // second alert when NSApp.terminate re-enters the delegate callback.
                 self?.isQuitWarningConfirmed = true
-                NSApp.terminate(nil)
+                AppDelegate.requestApplicationTermination()
             } else {
                 onCancel?()
             }
