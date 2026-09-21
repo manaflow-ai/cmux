ba257403611932cc1f011a6f5c31579d3b4055ce
austinpower1258 <austinwang115@gmail.com>
2026-09-20 18:07:36 -0700

    style: remove trailing blank line from portal reasons

--- Sources/TerminalPortalReconciliationReasons.swift
+++ Sources/TerminalPortalReconciliationReasons.swift
@@ -4,4 +4,3 @@ struct TerminalPortalReconciliationReasons: OptionSet {
     static let bindingRequired = Self(rawValue: 1 << 0)
     static let flushPendingManualSizeReport = Self(rawValue: 1 << 1)
 }
-
