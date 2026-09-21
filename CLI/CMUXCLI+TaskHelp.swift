781777d9bf2a4bf68adbf1b4caa6fbe82838666a
Leo <cheerleaderleo@outlook.com>
2026-09-21 03:21:21 -0700

    Expose Glaeda execution exchange in CMUX help

--- CLI/CMUXCLI+TaskHelp.swift
+++ CLI/CMUXCLI+TaskHelp.swift
@@ -297,6 +297,7 @@ extension CMUXCLI {
         return """
         events [--after <seq>] [--cursor-file <path>] [--name <event>] [--category <category>] [--reconnect] [--limit <n>] [--no-ack] [--no-heartbeat]
         automation <list|show|test|enable|disable|logs|reload> [args]
+        glaeda <request|observe> [options]
         todo <add|list|check|uncheck|start|rm|clear> [args] [--workspace <id|ref|index>] [--window <id|ref|index>]
         send [--workspace <id|ref|index>] [--surface <id|ref|index>] [--window <id|ref|index>] <text>
         send-key [--workspace <id|ref|index>] [--surface <id|ref|index>] [--window <id|ref|index>] <key>
