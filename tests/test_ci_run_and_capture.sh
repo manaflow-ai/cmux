6e46ef99a2e0866d5c5d83f08b850e137c367f97
Leo <cheerleaderleo@outlook.com>
2026-09-21 10:41:50 -0700

    Record cancellation command pid correctly

--- tests/test_ci_run_and_capture.sh
+++ tests/test_ci_run_and_capture.sh
@@ -76,7 +76,7 @@ SECONDS=0
 /bin/bash "$ROOT_DIR/scripts/ci/run-and-capture.sh" "$cancel_dir/capture.log" \
   /bin/bash -c '
     trap "echo term-forwarded >\"$1/term\"; exit 42" TERM
-    echo $ >"$1/command.pid"
+    echo $$ >"$1/command.pid"
     sleep 60 &
     echo $! >"$1/grandchild.pid"
     wait
