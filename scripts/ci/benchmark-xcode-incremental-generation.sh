1ef6e4137e878a857119f3a06c56f9a83fb3c226
Leo <cheerleaderleo@outlook.com>
2026-09-21 17:16:32 -0700

    ci: fix canary build log initialization

--- scripts/ci/benchmark-xcode-incremental-generation.sh
+++ scripts/ci/benchmark-xcode-incremental-generation.sh
@@ -122,7 +122,7 @@ resolve_packages() {
 }
 
 build_all() {
-  local arm="$1" wt="$2" dd="$3" log="$artifact_dir/${arm}.log"
+  local arm="$1" wt="$2" dd="$3"\n  local log="$artifact_dir/${arm}.log"
   local started finished wall
   : > "$log"
   mkdir -p "$dd" "$cas"
