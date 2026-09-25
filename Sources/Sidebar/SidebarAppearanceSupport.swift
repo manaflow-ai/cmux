960a5b0f0f2e51a3bcfb27f32c205598cb282404
Austin Wang <38676809+austinywang@users.noreply.github.com>
2026-09-25 02:57:50 -0700

    Fix sidebar accent resolution and palette compile error

--- Sources/Sidebar/SidebarAppearanceSupport.swift
+++ Sources/Sidebar/SidebarAppearanceSupport.swift
@@ -116,22 +116,7 @@ func titlebarControlForegroundNSColor(opacity: CGFloat, appearance: WindowAppear
 }
 
 func cmuxAccentNSColor(for colorScheme: ColorScheme) -> NSColor {
-    switch colorScheme {
-    case .dark:
-        return NSColor(
-            srgbRed: 0,
-            green: 145.0 / 255.0,
-            blue: 1.0,
-            alpha: 1.0
-        )
-    default:
-        return NSColor(
-            srgbRed: 0,
-            green: 136.0 / 255.0,
-            blue: 1.0,
-            alpha: 1.0
-        )
-    }
+    WindowAppearanceSnapshot.resolvedColor(.controlAccentColor, for: colorScheme)
 }
 
 func cmuxAccentNSColor(for appAppearance: NSAppearance?) -> NSColor {
